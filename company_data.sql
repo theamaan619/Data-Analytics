943c6faeb643163e1499be323b2e58987f91101b
Phenomenals619 <thephenomenals619@gmail.com>
2025-01-20 13:04:10 +0530

    sql1

---
+++ company_data.sql
@@ -0,0 +1,15 @@
+SELECT * FROM company.employees;
+
+SELECT DISTINCT dep_name
+FROM employees
+LIMIT 0, 10;
+
+SELECT *
+FROM employees
+WHERE hire_date < '2020-01-01';
+
+SELECT *
+FROM employees
+WHERE salary > 50000;
+
+
